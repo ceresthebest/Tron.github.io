b11153f8ea20f3cae144b83c868b6841a7ff150d
ceresthebest <67869485+ceresthebest@users.noreply.github.com>
2020-07-05 18:27:19 +0200

    Prima Pubblicazione
    Tron

---
+++ bike.js
@@ -0,0 +1,266 @@
+﻿function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function () {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    this.crashWithR = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        //confronta se il blue colpisce orange
+        //for (var i = 0; i < arrblue.length; i++) {
+        //    if (otherleft == arrblue[i][0] && othertop == arrblue[i][1]) {
+        //        crash = true;
+        //        myGameArea.stop();
+        //    }
+        //}
+
+        var crash = false;
+        for (var i = 0; i < arrblue.length; i++) {
+            if (otherleft - 10 == arrblue[i][0] && othertop - 5 == arrblue[i][1] && blueGamePiece.x != 10 && blueGamePiece.y != 10) {
+                crash = true;
+                myGameArea.stop();
+                return crash;
+
+            }
+        }
+
+
+        if (otherleft == 0 || othertop == 0 || otherleft == 300 || othertop == 150) {
+            crash = true;
+            myGameArea.stop();
+            return crash;
+
+        }
+
+       else if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+
+        }
+        else {
+
+            crash = true;
+            myGameArea.stop();
+
+        }
+        return crash;
+    }
+
+
+    this.crashWithY = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        //confronta se il blue colpisce orange
+        //for (var i = 0; i < arrblue.length; i++) {
+        //    if (otherleft == arrblue[i][0] && othertop == arrblue[i][1]) {
+        //        crash = true;
+        //        myGameArea.stop();
+        //    }
+        //}
+
+        var crash = false;
+        for (var i = 0; i < arrorange.length; i++) {
+            if (otherleft - 10 == arrorange[i][0] && othertop - 5 == arrorange[i][1] && orangeGamePiece.x != 280 && orangeGamePiece.y != 130) {
+                crash = true;
+                myGameArea.stop();
+                return crash;
+
+            }
+        }
+
+
+        if (otherleft == 0 || othertop == 0 || otherleft == 300 || othertop == 150) {
+            crash = true;
+            myGameArea.stop();
+            return crash;
+        }
+
+       else if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+            
+        }
+        else {
+            crash = true;
+            myGameArea.stop();
+
+        }
+        return crash;
+    }
+
+    this.crashWithRR = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        //confronta se il blue colpisce orange
+        //for (var i = 0; i < arrblue.length; i++) {
+        //    if (otherleft == arrblue[i][0] && othertop == arrblue[i][1]) {
+        //        crash = true;
+        //        myGameArea.stop();
+        //    }
+        //}
+
+        var crash = false;
+        for (var i = 0; i < arrblue.length-1; i++) {
+            if (myleft == arrblue[i][0] && mytop == arrblue[i][1] && blueGamePiece.x != 10 && blueGamePiece.y != 10) {
+               
+                crash = true;
+                myGameArea.stop();
+                return crash;
+
+            }
+        }
+
+
+        return crash;
+    }
+
+
+    this.crashWithYY = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        //confronta se il blue colpisce orange
+        //for (var i = 0; i < arrblue.length; i++) {
+        //    if (otherleft == arrblue[i][0] && othertop == arrblue[i][1]) {
+        //        crash = true;
+        //        myGameArea.stop();
+        //    }
+        //}
+
+        var crash = false;
+        for (var i = 0; i < arrorange.length-1; i++) {
+            if (myleft == arrorange[i][0] && mytop == arrorange[i][1] && orangeGamePiece.x != 280 && orangeGamePiece.y != 130) {
+         
+                crash = true;
+                myGameArea.stop();
+                return crash;
+
+            }
+        }
+
+
+        return crash;
+    }
+
+
+}
+
+
+function moveright(type) {
+
+    //blueGamePiece.x += 1;
+    if (type == 'R') {
+
+        document.getElementById("left").disabled = true;
+        document.getElementById("up").disabled = false;
+        document.getElementById("down").disabled = false;
+        document.getElementById("right").disabled = false;
+
+        blueGamePiece.speedY = 0;
+
+        blueGamePiece.speedX = 1;
+        
+ 
+    }
+    else {
+
+        orangeGamePiece.speedY = 0;
+
+        orangeGamePiece.speedX = 1;
+    }
+ 
+}
+function moveup(type) {
+    
+    //blueGamePiece.y = -1;
+    if (type == 'R') {
+        document.getElementById("down").disabled = true;
+        document.getElementById("up").disabled = false;
+        document.getElementById("left").disabled = false;
+        document.getElementById("right").disabled = false;
+
+        blueGamePiece.speedX = 0;
+        blueGamePiece.speedY = -1;
+
+    }
+    else {
+        orangeGamePiece.speedX = 0;
+        orangeGamePiece.speedY = -1;
+    }
+
+}
+function moveleft(type) {
+
+    if (type == 'R') {
+        document.getElementById("right").disabled = true;
+        document.getElementById("up").disabled = false;
+        document.getElementById("left").disabled = false;
+        document.getElementById("down").disabled = false;
+        blueGamePiece.speedY = 0;
+        blueGamePiece.speedX = -1;
+
+    }
+    else {
+ 
+                orangeGamePiece.speedY = 0;
+                orangeGamePiece.speedX = -1;
+      
+           
+
+    }
+    //blueGamePiece.x -= 1;
+
+}
+function movedown(type) {
+
+    if (type == 'R') {
+        document.getElementById("up").disabled = true;
+        document.getElementById("down").disabled = false;
+        document.getElementById("left").disabled = false;
+        document.getElementById("right").disabled = false;
+        blueGamePiece.speedX = 0;
+        blueGamePiece.speedY = 1;
+
+    }
+    else {
+        orangeGamePiece.speedX = 0;
+        orangeGamePiece.speedY = 1;
+    }
+    //blueGamePiece.y -= 1;
+    //blueGamePiece.x -= 2;
+
+}
+
+
---
+++ board.js
@@ -0,0 +1,263 @@
+﻿
+var blueGamePiece, orangeGamePiece;
+var pos = '';
+var giocatori = '';
+function startGame(players) {
+
+     document.getElementById("section_one").style.display = "block";
+     document.getElementById("section_one/slide_one").style.display = "block";
+    document.getElementById("btn-home").style.display = "block";
+    document.getElementById("btn-restart").style.display = "block";
+    blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+    orangeGamePiece = new component(10, 5, "orange", 280, 130);
+    giocatori = players;
+    myGameArea.start(players);
+    
+}
+
+
+var myGameArea = {
+
+    canvas: document.createElement("canvas"),
+    start: function (players) {
+	document.getElementById("section_one").style.display = "block";
+	document.getElementById("header").style.display = "none";
+        this.context = this.canvas.getContext("2d");
+
+        document.body.insertBefore(this.canvas, document.body.childNodes[6]);
+        this.interval = setInterval(updateGameArea, 20);
+        if (players == '2') {
+            document.getElementById("player").style.display = "block";
+            document.getElementById("blue").style.display = "inline-flex";
+            document.getElementById("orange").style.display = "inline-flex";
+            document.getElementById("player2").style.display = "inline-flex";
+
+            window.addEventListener('keydown', function (e) {
+                myGameArea.key = e.keyCode;
+            })
+            window.addEventListener('keyup', function (e) {
+                myGameArea.key = false;
+            })
+        }
+        else {
+            document.getElementById("blue").style.display = "inline-flex";
+            document.getElementById("com").style.display = "inline-flex";
+            myGameArea.key = 'Null';
+            if (myGameArea.key == 'Null') {
+                this.interval = setInterval(randompos, 200);
+
+            }
+        }
+      
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+	stop : function() {
+        clearInterval(this.interval);
+    } 
+}
+
+var arrblue = Array.from(Array(2), () => new Array(4));
+var arrorange = Array.from(Array(2), () => new Array(4));
+var i = 0;
+var j = 0;
+
+function updateGameArea() {
+    if (blueGamePiece.crashWithR(orangeGamePiece) /*&& orangeGamePiece.x != 280 && orangeGamePiece.y != 130*/) {
+        i++;
+        blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+        orangeGamePiece = new component(10, 5, "orange", 280, 130);
+        myGameArea.stop();
+        myGameArea.clear();
+
+        document.getElementById("result2").style.display = "block";
+        $("#joystick").addClass("disabledbutton");
+
+
+            if (typeof (Storage) !== "undefined") {
+                // Store
+                sessionStorage.setItem("result", i);
+                // Retrieve
+                document.getElementById("highscoresblue").innerHTML = sessionStorage.getItem("result");
+            } else {
+                i = 0;
+                sessionStorage.setItem("result", i);
+
+                document.getElementById("highscoresblue").innerHTML = "Sorry, your browser does not support Web Storage...";
+            }
+        
+
+    }
+    else if (orangeGamePiece.crashWithY(blueGamePiece)/* && blueGamePiece.x != 10 && blueGamePiece.y != 10*/) {
+
+        j++;
+        blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+        orangeGamePiece = new component(10, 5, "orange", 280, 130);
+        myGameArea.stop();
+        myGameArea.clear();
+
+        document.getElementById("result1").style.display = "block";
+        $("#joystick").addClass("disabledbutton");
+
+        if (typeof (Storage) !== "undefined") {
+            // Store
+            sessionStorage.setItem("result", j);
+            // Retrieve
+            document.getElementById("highscoresorange").innerHTML = sessionStorage.getItem("result");
+        } else {
+            j = 0;
+            sessionStorage.setItem("result", j);
+
+            document.getElementById("highscoresorange").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+
+    }                   
+    else if (blueGamePiece.crashWithRR(blueGamePiece) /*&& orangeGamePiece.x != 280 && orangeGamePiece.y != 130*/) {
+
+        i++;
+        blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+        orangeGamePiece = new component(10, 5, "orange", 280, 130);
+        myGameArea.stop();
+        myGameArea.clear();
+        document.getElementById("result1").style.display = "block";
+        $("#joystick").addClass("disabledbutton");
+
+        if (typeof (Storage) !== "undefined") {
+            // Store
+            sessionStorage.setItem("result", i);
+            // Retrieve
+            document.getElementById("highscoresblue").innerHTML = sessionStorage.getItem("result");
+        } else {
+            i = 0;
+            sessionStorage.setItem("result", i);
+
+            document.getElementById("highscoresblue").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+    }
+    else if (orangeGamePiece.crashWithYY(orangeGamePiece)/* && blueGamePiece.x != 10 && blueGamePiece.y != 10*/) {
+
+        i++;
+        blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+        orangeGamePiece = new component(10, 5, "orange", 280, 130);
+        myGameArea.stop();
+        myGameArea.clear();
+        document.getElementById("result2").style.display = "block";
+        $("#joystick").addClass("disabledbutton");
+
+    
+
+
+        if (typeof (Storage) !== "undefined") {
+            // Store
+            sessionStorage.setItem("result", i);
+            // Retrieve
+            document.getElementById("highscoresblue").innerHTML = sessionStorage.getItem("result");
+        } else {
+            i = 0;
+            sessionStorage.setItem("result", i);
+
+            document.getElementById("highscoresblue").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+    }
+	else{
+    //myGameArea.clear();
+    //blueGamePiece.x += 1;
+    if (myGameArea.key && myGameArea.key == 37) {
+        if (pos == "" || pos != 'R') {
+            moveleft('Y');
+            pos = 'L';
+        }}
+         if (myGameArea.key && myGameArea.key == 39) {
+             if (pos == "" || pos != 'L') {
+
+                 moveright('Y');
+                 pos = 'R';
+             }         }
+
+         if (myGameArea.key && myGameArea.key == 38) {
+             if (pos == "" || pos != 'D') {
+
+                 moveup('Y');
+                 pos = 'U';
+             }         }
+         if (myGameArea.key && myGameArea.key == 40) {
+             if (pos == "" || pos != 'U') {
+
+                 movedown('Y');
+                 pos = 'D';
+             }         }
+
+    orangeGamePiece.newPos();
+    //orangeGamePiece.x -= 1;
+    //orangeGamePiece.y += 1;
+         blueGamePiece.newPos();
+
+         blueGamePiece.update();
+         arrblue.push([blueGamePiece.x, blueGamePiece.y]);
+         orangeGamePiece.update();
+         arrorange.push([orangeGamePiece.x, orangeGamePiece.y]);
+
+    }
+
+
+}
+
+function home() {
+    document.getElementById("header").style.display = "block";
+    document.getElementById("section_one").style.display = "none";
+    document.getElementById("section_one/slide_one").style.display = "none";
+    i = 0;
+    $("#joystick").removeClass("disabledbutton");
+
+}
+
+
+function restart() {
+    document.getElementById("results").style.display = "none";
+    myGameArea.stop();
+    blueGamePiece = new component(10, 5, "#005bd1", 10, 10);
+    orangeGamePiece = new component(10, 5, "orange", 280, 130);
+    myGameArea.clear();
+    document.getElementById("results").style.display = "block";
+    document.getElementById("result1").style.display = "none";
+    document.getElementById("result2").style.display = "none";
+    myGameArea.start(giocatori);
+    $("#joystick").removeClass("disabledbutton");
+
+}
+function randompos() {
+    var keys = ['37', '39', '38', '40'];
+    var rnd = keys[Math.floor(Math.random() * keys.length)];
+    switch (rnd) {
+        case '37':
+            if (pos == "" || pos != 'R') {
+                moveleft('Y');
+                pos = 'L';
+            }
+
+            break;
+        case '39':
+            if (pos == "" || pos != 'L') {
+
+                moveright('Y');
+                pos = 'R';
+            }
+            break;
+        case '38':
+            if (pos == "" || pos != 'D') {
+
+                moveup('Y');
+                pos = 'U';
+            }
+            break;
+        case '40':
+            if (pos == "" || pos != 'U') {
+
+                movedown('Y');
+                pos = 'D';
+            }
+            break;
+        default:
+    }
+}
